528a987f8d5a91ffc029d6e9c04da15a2aa516cb
NinaLukashenko <nina.lucashenco@gmail.com>
2018-11-01 14:44:37 +0200

    Section Sweets is completed

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,9 @@
 function showMoreCakes() {
 	document.getElementById("additional-cakes").style.display = "block";
-	document.getElementById("button-moreCakes").style.display = "none";
+	document.getElementById("button-showMoreCakes").style.display = "none";
+}
+
+function showMoreSweets() {
+	document.getElementById("additional-sweets").style.display = "block";
+	document.getElementById("button-showMoreSweets").style.display = "none";
 }
